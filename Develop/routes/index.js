d9c346f9fced622b8f7f3a6acde754925234a2b4
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-29 17:38:02 -0500

    assignment completed on 24th

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
 
 router.use('/api', apiRoutes);
 
@@ -10,5 +14,12 @@ router.use('/api', apiRoutes);
 router.use((req, res) => {
     res.send("<h1>Wrong Route!</h1>")
 });
+
+
+app.post('/login')
+
+// listening for server creating a server
+
+
 
 module.exports = router;
